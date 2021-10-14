d425271e6e1ff62b93336df7acafc00d2025c5ed
K8sCat <k8scat@gmail.com>
2021-10-14 09:31:45 +0800

    optimize: config loader

--- gigrator/config.py
+++ gigrator/config.py
@@ -4,9 +4,15 @@ link: https://github.com/k8scat/gigrator.git
 """
 import yaml
 
+# https://pyyaml.org/wiki/PyYAMLDocumentation
+try:
+    from yaml import CLoader as Loader
+except ImportError:
+    from yaml import Loader
+
 
 def load_config(cfg_file: str) -> dict:
     with open(cfg_file, "r") as f:
-        cfg = yaml.load(f)
+        cfg = yaml.load(f, Loader=Loader)
 
     return cfg
